021d432131307c098694a2685f74986a7713131f
chandru <chandrukongari@gmail.com>
2023-03-03 13:36:40 +0530

    adding openAi api

--- popup.js
+++ popup.js
@@ -1,26 +1,29 @@
 // Get the current tab's URL
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentTabUrl = tabs[0].url;
-    var api_key = "SMMRY_API_KEY"; // from SMMRY website
-    var smmry_url = "https://api.smmry.com?SM_API_KEY=" + api_key + "&SM_LENGTH=5&SM_URL=" + encodeURIComponent(currentTabUrl);
-    var summary = "";
+    var api_key = "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"; // from OpenAI website
+    var openai_url = "https://api.openai.com/v1/engines/davinci/completions";
+    var summary="";
+    var data = {
+        "prompt": " Please Summarize the content of " + currentTabUrl,
+        "temperature": 0.8,
+        "max_tokens": 300,
+        
+    };
   
-    // Send a request to the SMMRY API to get the summary of the URL
+    // Send a request to the OpenAI API to get the summary of the URL
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", smmry_url, true);
+    xhr.open("POST", openai_url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", "Bearer " + api_key);
     xhr.onreadystatechange = function () {
       if (xhr.readyState == 4 && xhr.status == 200) {
         var response = JSON.parse(xhr.responseText);
-        summary = response.sm_api_content;
+        var summary = response.choices[0].text;
        
-        // Limit the summary 
-        summary = summary.substring(0, Math.min(summary.length, 1000));
-        summary = summary.substring(0, Math.max(summary.lastIndexOf("."), 500));
-        
         // Display the summary in the popup
         document.getElementById("summary").textContent = summary;
       }
     };
-    xhr.send();
-  });
-  
+    xhr.send(JSON.stringify(data));
+});
