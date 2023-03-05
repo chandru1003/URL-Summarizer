c073c2d7628fcad6735299b7989066ec23938439
chandru <chandrukongari@gmail.com>
2023-03-05 19:18:46 +0530

    added validation

--- popup.js
+++ popup.js
@@ -1,28 +1,35 @@
 // Get the current tab's URL
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var currentTabUrl =  tabs[0].url;
-    var api_key = "apikeys"; // from OpenAI website
-    var openai_url = "https://api.openai.com/v1/engines/davinci/completions";
-    var data = {
-        "prompt": " Please Summarize the content of " + currentTabUrl,
-        "temperature": 1.0,
-        "max_tokens": 200,
-        
-    };
-  
-    // Send a request to the OpenAI API to get the summary of the URL
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", openai_url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Authorization", "Bearer " + api_key);
-    xhr.onreadystatechange = function () {
+  var currentTabUrl = tabs[0].url;
+  var api_key = "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"; // from OpenAI website
+  var openai_url = "https://api.openai.com/v1/engines/davinci/completions";
+  var data = {
+      "prompt": "Summarize given url " + currentTabUrl,
+      "temperature": 0.1,
+      "max_tokens": 300,
+  };
+
+  // Send a request to the OpenAI API to get the summary of the URL
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", openai_url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer " + api_key);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.readyState == 4 && xhr.status == 200) {
         var response = JSON.parse(xhr.responseText);
-        var summary = response.choices[0].text;
-       
-        // Display the summary in the popup
-        document.getElementById("summary").textContent = summary;
+        if (response.choices && response.choices[0] && response.choices[0].text) 
+        {
+          var summary = response.choices[0].text;
+          // Display the summary in the popup
+          document.getElementById("summary").textContent = summary;
+        } else {
+          console.error("Unable to parse summary from API response.");
+        }
+      } else {
+        console.error("Error response from API: " + xhr.status);
       }
-    };
-    xhr.send(JSON.stringify(data));
+    }
+  };
+  xhr.send(JSON.stringify(data));
 });
